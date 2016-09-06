7c69c93db29e2bd49cef8617bc4a5e85bc9a39d7
oteroweb <root@24093.s.t4vps.eu>
2016-09-06 10:42:34 +0300

    todo Listo

--- src/ApiBundle/Controller/MemberController.php
+++ src/ApiBundle/Controller/MemberController.php
@@ -1,6 +1,7 @@
 <?php
 
 namespace ApiBundle\Controller;
+use Symfony\Component\Security\Core\Exception\AccessDeniedException;
 
 use ApiBundle\Controller\BaseController;
 use ApiBundle\Entity\User;
@@ -20,9 +21,12 @@ class MemberController extends BaseController
      */
     public function getAllAction()
     {
+        if (false === $this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw new AccessDeniedException();
+        }
         $users = $this->getDoctrine()->getRepository('ApiBundle:User')->findAll();
         if (!$users) {
-            $response = new Response('no user');
+            $response = new Response(json_encode('no user in database'));
              return $response;
         }
         $data = array('users' => array());
@@ -38,6 +42,9 @@ class MemberController extends BaseController
      */
     public function getOneAction($id)
     {
+        if (false === $this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw new AccessDeniedException();
+        }
         $user = $this->getDoctrine()->getRepository('ApiBundle:User')->find($id);
         if (!$user) {
             throw $this->createNotFoundException(sprintf(
@@ -58,6 +65,9 @@ class MemberController extends BaseController
 
     public function newAction(Request $request)
     {
+        if (false === $this->get('security.context')->isGranted('IS_AUTHENTICATED_FULLY')) {
+            throw new AccessDeniedException();
+        }
         $data = json_decode($request->getContent(), true);
         $user = new User();
         $form = $this->createForm(new UserType(), $user);
@@ -116,6 +126,20 @@ class MemberController extends BaseController
         return $response;
     }
 
+   /**
+     * @Route("/user/{id}")
+     * @Method("DELETE")
+     */
+    public function deleteAction($id)
+    {
+        $User = $this->getDoctrine()->getRepository('ApiBundle:User')->find($id);
+        if ($User) { 
+            $em = $this->getDoctrine()->getManager();
+            $em->remove($User);
+            $em->flush();
+        }
+        return new Response(null, 204);
+    }
     private function serializeUser(User $user)
     {
         return array(
@@ -123,9 +147,6 @@ class MemberController extends BaseController
             'username' => $user->getUsername(),
             'firstname' => $user->getFirstname(),
             'lastname' => $user->getLastname(),
-            // 'avatarNumber' => $user->getAvatarNumber(),
-            // 'powerLevel' => $user->getPowerLevel(),
-            // 'tagLine' => $user->getTagLine(),
         );
     }
 }
